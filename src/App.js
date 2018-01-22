c308ca38087e33df39391f289dfd79c863969e10
Noah <noahmatsell@gmail.com>
2018-01-21 22:47:31 -0800

    routes working

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import NotFound from './NotFound'
 export default class App extends Component {
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = {
       photos: [],
       loading: true,
@@ -52,13 +53,13 @@ export default class App extends Component {
             <Navbar />
             <Switch>
               <Route exact path={`/`} 
-                render={ () =>  (this.state.loading) ? <p>Loading...</p> : <PhotoContainer data={this.state.photos}/>}/> 
+                render={ () =>  (this.state.loading) ? <p>Loading...</p> : <PhotoContainer query= {this.props.query} search={this.performSearch} data={this.state.photos}/>}/> 
               <Route path={`/cats`} 
-                render={ () =>  (this.state.loading) ? <p>Loading...</p> : <PhotoContainer search={this.performSearch} data={this.state.photos}/>}/> 
+                render={ () =>  (this.state.loading) ? <p>Loading...</p> : <PhotoContainer query={"cats"} search={this.performSearch} data={this.state.photos}/>}/> 
               <Route path={`/dogs`} 
-                render={ () => (this.state.loading) ? <p>Loading...</p> : <PhotoContainer search={this.performSearch} data={this.state.photos}/>}/> 
+                render={ () => (this.state.loading) ? <p>Loading...</p> : <PhotoContainer query={"dogs"} search={this.performSearch} data={this.state.photos}/>}/> 
               <Route path={`/computers`} 
-                render={ () => (this.state.loading) ? <p>Loading...</p> : <PhotoContainer search={this.performSearch} data={this.state.photos}/>}/> 
+                render={ () => (this.state.loading) ? <p>Loading...</p> : <PhotoContainer query={"computers"} search={this.performSearch} data={this.state.photos}/>}/> 
               <Route component={NotFound} />
             </Switch>
           </div>
--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -6,9 +6,11 @@ import NoPhotos from './NoPhotos'
 export default class PhotoContainer extends Component {
   constructor(props) {
     super(props);
+    console.log(this.props)
   }
   
   componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
     if (nextProps.query !== this.props.query) {
       this.props.search(nextProps.query);
     }
